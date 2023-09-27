4647986dc1095f19c08616559527518d8b2d474f
ADMINPC\papin <papin.maks2011@yandex.ru>
2023-09-27 22:41:42 +0300

    -_-

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,8 +1,10 @@
 from requests import Response
 import json.decoder
+from datetime import datetime
+
 
 class BaseCase():
-    def get_cookie (self, response: Response, cookie_name):
+    def get_cookie(self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
         return response.cookies[cookie_name]
 
@@ -18,3 +20,17 @@ class BaseCase():
 
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
         return response_as_dict[name]
+
+    def prepare_registrarion_data(self, email=None):
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
